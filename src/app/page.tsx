0f465179f4721166dbe9df5ce3bb2166cb01060a
mawencan <1819293933@qq.com>
2024-11-30 20:53:26 +0800

    feat(balance): 获取余额

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,30 @@ import { Button } from '@mui/material'
 import { useAppKit } from '@reown/appkit/react'
 import { WalletIcon } from './_components/walletIcon'
 import { useWalletMiddleEllipsis } from './_hooks/useWalletMiddleEllipsis'
+import { useBalance } from 'wagmi'
 
 export default function Page() {
   const { open } = useAppKit()
-  const { walletMiddleEllipsis } = useWalletMiddleEllipsis()
-
+  const { walletMiddleEllipsis, address } = useWalletMiddleEllipsis()
+  const { data: balance } = useBalance({
+    address,
+  })
   function handleConnectWallet() {
     open()
   }
   return (
     <div className="absolute inset-0 flex items-center justify-center">
-      <Button
-        variant="outlined"
-        onClick={handleConnectWallet}
-        className="normal-case!"
-      >
-        <WalletIcon className="mr-1"></WalletIcon>
-        {walletMiddleEllipsis}
-      </Button>
+      <div>
+        <Button
+          variant="outlined"
+          onClick={handleConnectWallet}
+          className="normal-case!"
+        >
+          <WalletIcon className="mr-1"></WalletIcon>
+          {walletMiddleEllipsis}
+        </Button>
+        {balance ? <div className="mt-2">balance: {balance.value}</div> : null}
+      </div>
     </div>
   )
 }
